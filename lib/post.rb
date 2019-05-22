087724dc387119e0fd861d0778071e77306e2003
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-22 16:33:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,4 +2,18 @@ class Post
   
   attr_accessor :author 
   
+
+  def initialize(title)
+    @title = title
+    # @@all << self
+  end
+
+  def name 
+    @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end

8c5717395804777c426789ed4ddddb71c1e0890b
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-22 14:33:46 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  # def songs
+  #   @songs
+  # end
+  
+  # def add_song(song)
+  #   @songs << self
+  #   song.artist = self
+  # end
+
+
+
+end

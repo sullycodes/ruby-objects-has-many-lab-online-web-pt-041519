d63f883a59a84a9aeaa145d01ccbd6cbb24ef0ed
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-22 17:03:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,20 +19,12 @@ class Artist
     song.artist = self
   end
 
-  # def add_song_by_name(name)
-  #   song = Song.new(name)
-  #   @songs << song
-  #   @@songs << song
-  #   song.author = self
-  # end
-  
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     @@songs << song
     song.artist = self
   end
-  
   
   def self.song_count
     @@songs.count
